d41140b81bc5d89dabb676e37b16ecf4e41e8d2e
arushi2601 <71923862+arushi2601@users.noreply.github.com>
2023-11-10 23:15:03 +0530

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using utils;
+using CRPair = System.Tuple<uint, uint>;
+
+namespace Database;
+
+public class Database
+{
+    private Dictionary<uint, CRPair?> _registered { get; } = new();
+
+
+    public Database()
+    {
+        Console.WriteLine("\n=====================Initializing Db=====================\n");
+    }
+
+    public CRPair? getCrPair(uint tId)
+    {
+        if (_registered.TryGetValue(tId, out CRPair? crPair))
+        {
+            return crPair;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public bool insert(uint tId, uint c, uint r)
+    {
+        if (_registered.ContainsKey(tId))
+        {
+            return false;
+        }
+
+        _registered.Add(tId, new CRPair(c, r));
+        return true;
+    }
+}
