3065d5cbb69ebc104af58e242aeec3557e3b0e4b
arushi2601 <71923862+arushi2601@users.noreply.github.com>
2023-11-11 17:57:57 +0530

    Add files via upload

---
+++ ZTUAV.cs
@@ -0,0 +1,11 @@
+﻿using Entities;
+
+// var db = new Database.Database();
+var UAV_dev1 = new UAV(11011, 8011, 99819, 100119);
+var UAV_dev2 = new UAV(11000, 8000, 99820, 100120);
+
+var gs = new GroundStation();
+UAV_dev1.Register(gs);
+UAV_dev2.Register(gs);
+
+UAV_dev1.Authenticate(ref gs, UAV_dev2);
